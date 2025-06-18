4e1da2a84cdd74005158fa9fe5b1e306f4f011ef
richyp <r@p>
2025-06-18 22:13:16 +0200

    Models and first attempt at loader script

--- bookrating_project/bookrating/models.py
+++ bookrating_project/bookrating/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+#Work id corresponds to goodbooks work_id
+#work_id refers to the book (regardless of edition/version)
+class Work(models.Model):
+    id               = models.IntegerField(primary_key=True)
+    title            = models.CharField(max_length=255)
+    authors          = models.ManyToManyField("Author", through="WorkAuthor")
+    original_year    = models.IntegerField(null=True, blank=True)
+    avg_rating       = models.DecimalField(max_digits=3, decimal_places=2)
+    ratings_count    = models.PositiveIntegerField()
+
+#BookEdition id corresponds to book_id in goodbooks
+#Book edition holds data for each separate version of a book
+class BookEdition(models.Model):
+    id             = models.IntegerField(primary_key=True)
+    work           = models.ForeignKey(Work, on_delete=models.CASCADE,
+                                       related_name="editions")
+    isbn           = models.CharField(max_length=13, null=True, blank=True)
+    isbn13         = models.CharField(max_length=13, null=True, blank=True)
+    language_code  = models.CharField(max_length=10, null=True, blank=True)
+    #Edition-specific popularity
+    ratings_count  = models.PositiveIntegerField()
+    avg_rating     = models.DecimalField(max_digits=3, decimal_places=2)
+
+class WorkAuthor(models.Model):
+    work   = models.ForeignKey(Work,   on_delete=models.CASCADE)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+
+#Table of authors
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class EditionTag(models.Model):
+    edition = models.ForeignKey(BookEdition, on_delete=models.CASCADE)
+    tag     = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    count   = models.PositiveIntegerField()
+
+class Rating(models.Model):
+    user_id = models.IntegerField()
+    edition = models.ForeignKey(BookEdition, on_delete=models.CASCADE)
+    rating  = models.PositiveSmallIntegerField()
