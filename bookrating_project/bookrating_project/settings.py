4784e624511ddd8da85e718d517c8dd1c2fab6f3
rinnychinny <richpemuk@gmail.com>
2025-06-29 09:48:35 +0200

    get secret_key from os.environ

--- bookrating_project/bookrating_project/settings.py
+++ bookrating_project/bookrating_project/settings.py
@@ -22,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Serve static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#b1l*35!!%&n7cpm#eiwjxb_5jklal%ee9zkqv-6+bk84)b-u5'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
